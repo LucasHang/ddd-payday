bca2df2a28fea670e2f48acba20d8ae92d70af4b
Lucas Hang <lucas.hhang@gmail.com>
2021-03-14 14:20:11 -0300

    Adicionado teste de sucesso da criação do domain userAge

--- src/modules/users/domain/tests/userAge.spec.ts
+++ src/modules/users/domain/tests/userAge.spec.ts
@@ -16,4 +16,19 @@ describe('UserAge domain', () => {
         expect(error.statusCode).toBe(StatusCodes.BAD_REQUEST);
         expect(error.message).toBe(`'Idade' deve ser no mínimo ${UserAge.minAge} anos`);
     });
+
+    it('Should return a instance of UserAge if a valid age was provided', () => {
+        const toCreateAge = 16;
+
+        const ageOrError = UserAge.create(toCreateAge);
+
+        expect(ageOrError.isRight()).toBeTruthy();
+
+        if (!ageOrError.isRight()) return;
+
+        const age = ageOrError.value;
+
+        expect(age).toBeInstanceOf(UserAge);
+        expect(age.value).toBe(toCreateAge);
+    });
 });
